ee30716f4ad0b4f70a387e8f1bf6c26e87786a0e
hreshi <hkale746@gmail.com>
2022-03-17 08:54:24 +0530

    Fetch page works

--- src/test/java/com/hreshi/AppTest.java
+++ src/test/java/com/hreshi/AppTest.java
@@ -1,23 +1,33 @@
 package com.hreshi;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 import org.junit.Test;
+
+import java.util.List;
 
 public class AppTest {
     @Test
     public void fetchMcq() {
         WebDriverManager.chromedriver().setup();
 
-
-        System.out.println("Fetching Page has started ...");
         String link= "https://codeforces.com/problemset";
-        WebDriver driver = new ChromeDriver();
+
+        WebDriver driver = new ChromeDriver((new ChromeOptions()).setHeadless(true));
         driver.get(link);
-        
+
+        WebElement table = driver.findElement(By.className("problems"));
+        List<WebElement> rows = driver.findElements(By.tagName("tr"));
+        for (WebElement element : rows) {
+            System.out.println (element.getText());
+        }
         driver.quit();
     }
 }
